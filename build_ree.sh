eb14378781f59376f932afe9507dad8cdc3a9ae6
Sam Kottler <shk@linux.com>
2015-05-09 15:34:09 -0400

    Refactor the build script to be more compartmentalized

--- build_ree.sh
+++ build_ree.sh
@@ -2,45 +2,59 @@
 
 set -e
 
-FULL_VERSION="1.8.7-2012.02"
-FULL_NAME="ruby-enterprise-${FULL_VERSION}"
+function init() {
+  FULL_VERSION="1.8.7-2012.02"
+  FULL_NAME="ruby-enterprise-${FULL_VERSION}"
+  TEMP_DIR=$(mktemp -d)
 
-TEMP_DIR=$(mktemp -d)
+  echo "Serving from ${TEMP_DIR}"
 
-cd $TEMP_DIR
+  cd /tmp
 
-rm -rf /tmp/tmp.*
+  python -m SimpleHTTPServer $PORT &
+}
 
-echo "Serving from ${TEMP_DIR}"
-cd /tmp
-python -m SimpleHTTPServer $PORT &
+function download_and_patch() {
+  init
 
-curl https://rubyenterpriseedition.googlecode.com/files/${FULL_NAME}.tar.gz -s -o - | tar zxf -
+  cd $TEMP_DIR
+  curl https://rubyenterpriseedition.googlecode.com/files/${FULL_NAME}.tar.gz -s -o - | tar zxf -
+  cd $FULL_NAME/source
+  curl -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
+  curl -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch
+  curl -o tcmalloc_declare_memalign_volatile.patch https://raw.githubusercontent.com/Genius/rubyenterpriseedition187-330/master/patches/tcmalloc_declare_memalign_volatile.patch
+  patch -p1 < 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
+  patch -p1 < 5384967a015be227e16af7a332a50d45e14ed0ad.patch
+  patch -p1 < tcmalloc_declare_memalign_volatile.patch
+}
 
-cd $FULL_NAME/source
-curl -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
-curl -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch
-curl -o tcmalloc_declare_memalign_volatile.patch https://raw.githubusercontent.com/Genius/rubyenterpriseedition187-330/master/patches/tcmalloc_declare_memalign_volatile.patch
-patch -p1 < 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
-patch -p1 < 5384967a015be227e16af7a332a50d45e14ed0ad.patch
-patch -p1 < tcmalloc_declare_memalign_volatile.patch
+function run_installer() {
+  DIRECTORY=$1
+  OUTPUT=$2
 
-cd ../
+  download_and_patch
 
-./installer --auto $TEMP_DIR/ruby-1.8.7 --no-dev-docs --no-tcmalloc
+  if [ -e $DIRECTORY ]; then
+    echo "Output directory ${DIRECTORY} already exists. Removing."
+    rm -rf $DIRECTORY
+  fi
 
-echo "Creating tarball without the '-build' suffix"
-mkdir -p /app/vendor
-mv $TEMP_DIR/ruby-1.8.7 /app/vendor/
-tar czvf /tmp/$FULL_NAME.tar.gz /app/vendor/ruby-1.8.7
+  cd $TEMP_DIR/$FULL_NAME
 
-echo "Creating tarball with the '-build' suffix"
-rm -rf /tmp/*
-mv /app/vendor/ruby-build-1.8.7 /tmp
-tar xzvf /tmp/ruby-build-1.8.7.tar.gz /tmp/ruby-build-1.8.7
+  ./installer --auto $DIRECTORY --no-dev-docs --no-tcmalloc
 
-while true
-do
-  sleep 60
-  echo "Still serving."
-done
+  tar czvf /tmp/${OUTPUT}.tar.gz $DIRECTORY
+}
+
+function listen_and_serve() {
+  while true
+  do
+    sleep 60
+    echo "Still serving."
+  done
+}
+
+run_installer /app/vendor/ruby-1.8.7 ruby-1.8.7
+run_installer /tmp/ruby-1.8.7 ruby-build-1.8.7
+
+listen_and_serve
