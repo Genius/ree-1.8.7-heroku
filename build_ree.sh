d24bee5f78336a263cb5b19e71a9bf51b16f8780
Sam Kottler <shk@linux.com>
2015-05-24 10:25:22 -0400

    update message about where SimpleHTTPServer is serving from and use patches from this repo

--- build_ree.sh
+++ build_ree.sh
@@ -7,7 +7,7 @@ function init() {
   FULL_NAME="ruby-enterprise-${FULL_VERSION}"
   TEMP_DIR=$(mktemp -d)
 
-  echo "Serving from ${TEMP_DIR}"
+  echo "Serving from /tmp"
 
   cd /tmp
 
@@ -20,9 +20,9 @@ function download_and_patch() {
   cd $TEMP_DIR
   curl https://rubyenterpriseedition.googlecode.com/files/${FULL_NAME}.tar.gz -s -o - | tar zxf -
   cd $FULL_NAME/source
-  curl -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
-  curl -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/Genius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch
-  curl -o tcmalloc_declare_memalign_volatile.patch https://raw.githubusercontent.com/Genius/rubyenterpriseedition187-330/master/patches/tcmalloc_declare_memalign_volatile.patch
+  curl -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://raw.githubusercontent.com/Genius/ree-1.8.7-heroku/master/patches/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
+  curl -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://raw.githubusercontent.com/Genius/ree-1.8.7-heroku/master/patches5384967a015be227e16af7a332a50d45e14ed0ad.patch
+  curl -o tcmalloc_declare_memalign_volatile.patch https://raw.githubusercontent.com/Genius/ree-1.8.7-heroku/master/patches/tcmalloc_declare_memalign_volatile.patch
   patch -p1 < 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch
   patch -p1 < 5384967a015be227e16af7a332a50d45e14ed0ad.patch
   patch -p1 < tcmalloc_declare_memalign_volatile.patch
